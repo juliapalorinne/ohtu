f0a47500f802c2bbcfd6b2f1e6d43c7ff03ec4a0
juliapalorinne <julia.palorinne@helsinki.fi>
2020-11-30 23:58:21 +0200

    viikko 5

---
+++ viikko5/LaskinFX11/src/main/java/laskin/Summa.java
@@ -0,0 +1,45 @@
+
+package laskin;
+
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+
+public class Summa extends Komento {
+    private int edellinen;
+    
+    public Summa(TextField tuloskentta, TextField syotekentta, Button nollaa, Button undo, Sovelluslogiikka sovellus) {
+        super(tuloskentta, syotekentta,  nollaa, undo, sovellus);
+    }
+
+    @Override
+    public void suorita() {
+        int arvo = 0;
+ 
+        try {
+            arvo = Integer.parseInt(syotekentta.getText());
+        } catch (Exception e) {
+        }
+        
+        edellinen = sovellus.tulos();
+        sovellus.plus(arvo);
+        
+        int laskunTulos = sovellus.tulos();
+        
+        syotekentta.setText("");
+        tuloskentta.setText("" + laskunTulos);
+
+        if ( laskunTulos==0) {
+            nollaa.disableProperty().set(true);
+        } else {
+            nollaa.disableProperty().set(false);
+        }
+        undo.disableProperty().set(false);        
+    }  
+
+    @Override
+    public void peru() {
+        syotekentta.setText("");
+        tuloskentta.setText("" + edellinen);
+    }
+}
